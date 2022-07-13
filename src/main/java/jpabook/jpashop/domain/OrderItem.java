098f89be2c51ef0ca87dca8e3c35ebcbf71d5083
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-07-13 21:28:42 +0900

    2022-07-12 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 (엔티티 클래스 개발1)_2

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.domain.item.Item;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,6 +16,8 @@ public class OrderItem {
     @Column(name = "order_item_id")
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "itme_id")
     private Item item;
 
     @ManyToOne
