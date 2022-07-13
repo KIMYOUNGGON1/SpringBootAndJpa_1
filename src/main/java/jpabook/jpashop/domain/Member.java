f8a5f4140139552d0433c23d7e36405661659f12
KIMYOUNGGON1 <dudrhs950@naver.com>
2022-07-13 12:33:59 +0900

    2022-07-12 실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발 (엔티티 클래스 개발1)_1

---
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter @Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded // Address class의 @Embeddable annotation과 같은 역할 둘 중 하나만 있어도 된다.
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // 매핑되었을 뿐 연관관계의 주인은 Order class의 member다.
+    private List<Order> orders = new ArrayList<>();
+
+
+}
